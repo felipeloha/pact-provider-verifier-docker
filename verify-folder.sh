30505e7c4dbe5938ad7d6326ecf58add724bd624
felipeloha <36071293+felipeloha@users.noreply.github.com>
2019-11-01 08:31:21 +0100

    Folder verifier

---
+++ verify-folder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "### verifying pacts ###"
+rm -f pact.report || true
+touch pact.report || true
+
+for filename in $1/*.json; do
+    pact-provider-verifier $filename $2 >> pact.report
+done
+
+echo "#### pacts verified ####"
+cat pact.report
+
+ $filename $2 >> pact.report
+done
+
+echo "#### pacts verified ####"
+cat pact.report
+
+
+#usage: ./verify-folder.sh pacts "--provider-base-url http://elixir-base:80 --custom-provider-header \"Authorization: Bearer sometoken\""
